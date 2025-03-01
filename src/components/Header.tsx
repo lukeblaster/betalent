39b7d0b1240173f528adef96ebb45bd9d3c1fbfe
Lucas Soares <soaresl472@gmail.com>
2025-03-01 14:52:20 -0300

    chore: add db.json file

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function Header({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex items-center min-h-16 bg-white px-regular-20 md:px-regular-28 shadow-2">
-      {children}
-    </div>
-  );
-}
--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export function Logo() {
-  return (
-    <h1 className="h1 font-medium">
-      <p>
-        <img src="/images/BeTalent.svg" />
-      </p>
-    </h1>
-  );
-}
